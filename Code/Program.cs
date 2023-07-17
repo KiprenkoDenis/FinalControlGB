65f1e914d7c123bcb62f547656bc1360ee0779ad
Denis_Kiprenko <deniskiprenko@yandex.ru>
2023-07-18 00:31:25 +1000

    Добавил код программы

---
+++ Code/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите элементы массива через запятую: ");
+        string input = Console.ReadLine();
+        string[] originalArray = input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        string[] resultArray = Array.FindAll(originalArray, item => item.Length <= 3);
+
+        Console.Write($"[{string.Join(", ", originalArray)}] -> ");
+        Console.WriteLine($"[{string.Join(", ", resultArray)}]");
